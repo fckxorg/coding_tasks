597e0a55b79fa2d08d1d762a84455131fc9e082d
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2019-09-03 18:07:48 +0300

    std_func_realization

---
+++ std_functions_realization/cmake-build-debug/CMakeFiles/std_functions_realization.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/std_functions_realization.dir/main.cpp.o"
+  "std_functions_realization"
+  "std_functions_realization.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/std_functions_realization.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
